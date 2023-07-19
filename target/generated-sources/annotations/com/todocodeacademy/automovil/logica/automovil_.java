ed69fbdc0f8dc06c0b1f8f11718a2843050d5181
oscarcipi <108748579+oscarcipi@users.noreply.github.com>
2023-07-19 11:46:02 +0200

    Add files via upload

---
+++ target/generated-sources/annotations/com/todocodeacademy/automovil/logica/automovil_.java
@@ -0,0 +1,20 @@
+package com.todocodeacademy.automovil.logica;
+
+import automovil.logica.automovil;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-05-31T13:50:49", comments="EclipseLink-2.7.7.v20200504-rNA")
+@StaticMetamodel(automovil.class)
+public class automovil_ { 
+
+    public static volatile SingularAttribute<automovil, String> marca;
+    public static volatile SingularAttribute<automovil, String> motor;
+    public static volatile SingularAttribute<automovil, Integer> cantPuertas;
+    public static volatile SingularAttribute<automovil, String> color;
+    public static volatile SingularAttribute<automovil, Integer> id;
+    public static volatile SingularAttribute<automovil, String> modelo;
+    public static volatile SingularAttribute<automovil, String> patente;
+
+}
